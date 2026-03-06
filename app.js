b2603a79a24e73de016928454c5a7faab6ac42cb
Satoru_Gojo077 <huseyintabak07@hotmail.com>
2026-03-06 14:42:42 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T11:42:34.141Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T11:42:43.469Z
+Commit: Resolve null pointer exception in user service
