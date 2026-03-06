8d9ef3e0a30abb1daf30b556ef7419df6de4b495
Satoru_Gojo077 <huseyintabak07@hotmail.com>
2026-03-06 14:41:56 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T11:39:32.805Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T11:41:57.269Z
+Commit: Fix memory leak in event handlers
